8885c26d4f770d777fc461e635ed16ab99960a18
Matthew Langwell <mlangwell9311@gmail.com>
2023-11-07 20:02:23 -0500

    made it so that the targets fade in and out

--- src/app/target/target.component.ts
+++ src/app/target/target.component.ts
@@ -1,12 +1,13 @@
-import { Component, ElementRef, HostListener, Input } from '@angular/core';
+import { Component, ElementRef, HostListener, Input, OnDestroy, OnInit } from '@angular/core';
 import { AppService } from '../app.service';
+import { take, timer } from 'rxjs';
 
 @Component({
   selector: 'app-target',
   templateUrl: './target.component.html',
   styleUrls: ['./target.component.scss']
 })
-export class TargetComponent {
+export class TargetComponent implements OnInit {
   @HostListener('click', ['$event.target'])
   onClick(btn: HTMLElement) {
     this._elRef.nativeElement.remove();
@@ -14,5 +15,9 @@ export class TargetComponent {
   }
 
   constructor(private _appService: AppService, private _elRef: ElementRef) {
+  }
+
+  ngOnInit(): void {
+    timer(3000).pipe(take(1)).subscribe(() => this._elRef.nativeElement.remove())
   }
 }
